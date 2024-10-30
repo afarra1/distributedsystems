4c6419fbf4aa6584625d992a3fa2f5dcce5a2397
afarra1 <ahmedelfarra.619@gmail.com>
2024-10-30 20:48:51 +0300

    dashboard set up ui:

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -2,10 +2,32 @@ import React from 'react'
 import './stylesheets/Dashboard.css'
 
 
+const DashboardBox = ({ children, width, height, id }) => {
+  return (
+    <div className='dashboardboxcomponent' id={id} style={{ width: width, height: height }}>
+      {children}
+    </div>
+  )
+}
+
 export default function Dashboard() {
+  let name = "Robert Henry"
   return (
     <div className='DashboardWrapper'>
-        
+      <h3>Welcome Dr. {name}</h3>
+      <div className='DashboardRow' style={{marginTop:'1.25%'}}>
+        <DashboardBox id={"smallbox"} width={250} height={83.33}></DashboardBox>
+        <DashboardBox id={"smallbox"} width={250} height={83.33}></DashboardBox>
+      </div>
+      <div className='DashboardRow'>
+        <DashboardBox id={"smallbox"} width={'calc(500px + 2.5%)'} height={250}></DashboardBox>
+        <DashboardBox id={"smallbox"} width={450} height={250}></DashboardBox>
+      </div>
+      <div className='DashboardRow'>
+        <DashboardBox id={"smallbox"} width={'calc(500px + 2.5%)'} height={250}></DashboardBox>
+        <DashboardBox id={"smallbox"} width={450} height={250}></DashboardBox>
+      </div>
+
     </div>
   )
 }
